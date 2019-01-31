e7989c7f7a8e9c30dc9f9d1d458f46110fe3108e
DenysSyrbu <denis_s@dataforest.ai>
2019-01-31 15:22:32 +0200

    Add new project and models

--- python_test/models.py
+++ python_test/models.py
@@ -1,23 +1,4 @@
-from django.db import models
-from django.core.validators import RegexValidator
 
-
-
-class Client(models.Model):
-    client_name = models.CharField(max_length=150)
-    contact_name = models.CharField(max_length=150)
-    client_email = models.EmailField()
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-
-
-class Address_datails(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    street_name = models.CharField(max_length=100)
-    suburb = models.CharField(max_length=150)
-    postcode = models.IntegerField()
-    state = models.CharField(max_length=100)
 
 
 
